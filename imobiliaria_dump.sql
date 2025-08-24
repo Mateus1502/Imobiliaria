55608ccdd2a26bbff8a0e166cdba25b735dec0ac
Mateus Böhr <153296791+Mateus1502@users.noreply.github.com>
2025-08-24 17:32:50 -0300

    Dump sql

--- imobiliaria_dump.sql
+++ imobiliaria_dump.sql
@@ -15,7 +15,9 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+--Comandos para criação da Database
+CREATE DATABASE imobiliaria;
+USE imobiliaria;
 --
 -- Table structure for table `Clientes`
 --
